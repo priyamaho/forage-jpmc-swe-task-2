e9dffd9fafe643fe8e15bb25245c17054c5d23ef
priyamaho <priyankamahovia@gmail.com>
2023-06-10 15:54:33 -0500

    Task 2 changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import './App.css';
 
 /**
  * State declaration for <App />
+ * Changes done by Priyanka Kushwaha
  */
 interface IState {
   data: ServerRespond[],
+  showGraph:boolean,
 }
 
 /**
  * The parent element of the react app.
  * It renders title, button and Graph react element.
+ * Changes done by Priyanka Kushwaha.
  */
 class App extends Component<{}, IState> {
   constructor(props: {}) {
@@ -22,6 +25,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph:false,
     };
   }
 
@@ -29,18 +33,31 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+  if(this.state.showGraph){
+
     return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
+   * Changes done by Priyanka Kushwaha
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
+  let x = 0;
+  const interval = setInterval(() => {
+     DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
+      this.setState({ data:serverResponds,
+      showGraph:true
+       });
     });
+   x++;
+   if(x>1000){
+   clearInterval(interval);
+   }
+  },100);
   }
 
   /**
